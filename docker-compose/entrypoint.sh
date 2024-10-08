aab0b4ce96e22eec98bbac20e644691568adf720
abdoazmygad <a.gad@dopatravelapp.com>
2024-10-08 20:25:59 +0300

    update files

--- docker-compose/entrypoint.sh
+++ docker-compose/entrypoint.sh
@@ -5,18 +5,8 @@ if [ ! -f "vendor/autoload.php" ]; then
 fi
 
 if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
+    echo "Creating env file for env variables."
     cp .env.example .env
-    case "$APP_ENV" in
-    "local")
-        echo "Copying .env.example ... "
-        cp .env.example .env
-    ;;
-    "prod")
-        echo "Copying .env.prod ... "
-        cp .env.prod .env
-    ;;
-    esac
 else
     echo "env file exists."
 fi
