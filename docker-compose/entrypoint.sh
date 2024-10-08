bd52716ed9e63875bdd02419876abb2b5751a2f0
abdoazmygad <a.gad@dopatravelapp.com>
2024-10-08 20:12:38 +0300

    update entrypoint.sh

--- docker-compose/entrypoint.sh
+++ docker-compose/entrypoint.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
-# entrypoint.sh
 
-# Start the service
-composer install --no-interaction
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader
+fi
+
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env $APP_ENV"
+    cp .env.example .env
+    case "$APP_ENV" in
+    "local")
+        echo "Copying .env.example ... "
+        cp .env.example .env
+    ;;
+    "prod")
+        echo "Copying .env.prod ... "
+        cp .env.prod .env
+    ;;
+    esac
+else
+    echo "env file exists."
+fi
+
+# php artisan migrate
+php artisan clear
+php artisan optimize:clear
 php artisan migrate
-php artisan config:clear
-php artisan cache:clear
-php artisan config:cache
 
-# Execute the CMD passed from Dockerfile or docker-compose.yml
-exec "$@"
+# Fix files ownership.
+chown -R www-data .
+chown -R www-data /app/storage
+chown -R www-data /app/storage/logs
+chown -R www-data /app/storage/framework
+chown -R www-data /app/storage/framework/sessions
+chown -R www-data /app/bootstrap
+chown -R www-data /app/bootstrap/cache
+
+# Set correct permission.
+chmod -R 775 /app/storage
+chmod -R 775 /app/storage/logs
+chmod -R 775 /app/storage/framework
+chmod -R 775 /app/storage/framework/sessions
+chmod -R 775 /app/bootstrap
+chmod -R 775 /app/bootstrap/cache
+
+php-fpm -D
+nginx -g "daemon off;"
