b84e7c7d70e150b13310c12edd88aeb69e955526
abdoazmygad <a.gad@dopatravelapp.com>
2024-10-08 23:44:25 +0300

    fix ownership

--- docker-compose/entrypoint.sh
+++ docker-compose/entrypoint.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
-# entrypoint.sh
 
-# Start the service
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env variables."
+    cp .env.example .env
+else
+    echo "env file exists."
+fi
+
+
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-interaction 
+fi
+
+
 php artisan migrate
-php artisan config:clear
-php artisan cache:clear
-php artisan config:cache
+php artisan clear
+php artisan optimize:clear
+php artisan migrate
 
-# Execute the CMD passed from Dockerfile or docker-compose.yml
-exec "$@"
+# Fix files ownership.
+chown -R www-data .
+chown -R www-data /app/storage
+chown -R www-data /app/storage/logs
+chown -R www-data /app/storage/framework
+chown -R www-data /app/storage/framework/sessions
+chown -R www-data /app/bootstrap
+chown -R www-data /app/bootstrap/cache
+
+# Set correct permission.
+chmod -R 775 /app/storage
+chmod -R 775 /app/storage/logs
+chmod -R 775 /app/storage/framework
+chmod -R 775 /app/storage/framework/sessions
+chmod -R 775 /app/bootstrap
+chmod -R 775 /app/bootstrap/cache
+
+php-fpm -D
+nginx -g "daemon off;"
+
