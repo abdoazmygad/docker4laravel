b2041067ab4702eb92bf9a6f5b5e960dfe441cad
abdoazmygad <a.gad@dopatravelapp.com>
2024-10-08 20:17:32 +0300

    fix sh

--- docker-compose/entrypoint.sh
+++ docker-compose/entrypoint.sh
@@ -25,23 +25,3 @@ fi
 php artisan clear
 php artisan optimize:clear
 php artisan migrate
-
-# Fix files ownership.
-chown -R www-data .
-chown -R www-data /app/storage
-chown -R www-data /app/storage/logs
-chown -R www-data /app/storage/framework
-chown -R www-data /app/storage/framework/sessions
-chown -R www-data /app/bootstrap
-chown -R www-data /app/bootstrap/cache
-
-# Set correct permission.
-chmod -R 775 /app/storage
-chmod -R 775 /app/storage/logs
-chmod -R 775 /app/storage/framework
-chmod -R 775 /app/storage/framework/sessions
-chmod -R 775 /app/bootstrap
-chmod -R 775 /app/bootstrap/cache
-
-php-fpm -D
-nginx -g "daemon off;"
