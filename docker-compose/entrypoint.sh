36cfcae0e965167b59d0edba24db5e9a2540f850
abdoazmygad <a.gad@dopatravelapp.com>
2024-10-08 20:29:52 +0300

    create .env first then install

--- docker-compose/entrypoint.sh
+++ docker-compose/entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-if [ ! -f "vendor/autoload.php" ]; then
-    composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader
-fi
-
 if [ ! -f ".env" ]; then
     echo "Creating env file for env variables."
     cp .env.example .env
 else
     echo "env file exists."
 fi
+
+
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader
+fi
+
 
 # php artisan migrate
 php artisan clear
