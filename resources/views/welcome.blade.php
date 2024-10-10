f82ef1684d13209d5b4662db981af9733968e874
abdoazmygad <a.gad@dopatravelapp.com>
2024-10-10 21:54:32 +0800

    test pull code on machine properly

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Update</title>
+        <title>Laravel check change
+            
+        </title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
