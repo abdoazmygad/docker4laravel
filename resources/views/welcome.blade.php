51071b3dbe562a9a0facda0242be8011a8b149f4
abdoazmygad <a.gad@dopatravelapp.com>
2024-10-13 01:47:03 +0800

    create dockerized larravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel check change
+        <title>Dockerized Laravel Application
             
         </title>
 
